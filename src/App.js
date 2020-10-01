31a753e94be42281aea0af8e01f78c43e7185fdd
stevemwangi <sttevemwangi@gmail.com>
2020-10-01 11:48:20 +0300

    Completed the subtotoal component of the checkout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Checkout from './components/checkout/Checkout'; 
 
 import './App.css';
 
@@ -20,7 +21,7 @@ function App() {
         </Route>
         <Route path="/checkout">
           <Header />
-          <h1>Checkout page</h1>
+          <Checkout />           
         </Route>     
       </Switch>
       </div>        
--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,35 @@
 export const initialState = {
-    basket: [],
-}
+    basket: [
+        // {
+        //     id: '1234',
+        //     title: 'New Book',
+        //     price: 98.99,
+        //     rating: 5,
+        //     image: 'https://m.media-amazon.com/images/I/518DfIBtGzL._SY346_.jpg'
+        // }, 
+        // {
+        //     id: '12345',
+        //     title: 'New Book 2',
+        //     price: 108.99,
+        //     rating: 4,
+        //     image: 'https://m.media-amazon.com/images/I/518DfIBtGzL._SY346_.jpg'
+        // },
+        // {
+        //     id: '12346',
+        //     title: 'New Book 3',
+        //     price: 29.99,
+        //     rating: 3,
+        //     image: 'https://m.media-amazon.com/images/I/518DfIBtGzL._SY346_.jpg'
+        // }
+    ],
+    user: null,
+}; 
 
+export const getBasketTotal = (basket) => 
+basket?.reduce((amount, item) => item.price + amount, 0);
+ 
 const reducer = (state, action) => {
+    console.log(action); 
     switch(action.type) {        
         case 'ADD_TO_BASKET':
             console.log(action) ;             
@@ -10,8 +37,23 @@ const reducer = (state, action) => {
                 ...state, 
                 basket: [...state.basket, action.item]  
             };
-        case 'REMOVE_FROM_BASKET':             
-            return state; 
+        case 'REMOVE_FROM_BASKET':  
+
+            let newBasket = [...state.basket];            
+            const index = state.basket.findIndex(
+                basketItem => basketItem.id === action.id
+                );
+
+            if(index >=0 ) {
+              // It exists proceed to remove it 
+                newBasket.splice(index, 1);
+            } else {
+                console.warn('Cannot remove product (id: ${action.id}) as it does not exist in basket')
+            }
+            return {
+                ...state,
+                basket: newBasket
+            }; 
         default:
             return state;
     }
